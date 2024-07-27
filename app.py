39a1a3d070e1e14fd9074358983eb7ca95a25342
Pranav Hari <pranavbts2001@gmail.com>
2024-07-27 09:28:38 -0400

    Add form data

--- app.py
+++ app.py
@@ -30,24 +30,24 @@ def index():
 @app.route('/generate-cover-letter', methods=['POST'])
 def generate_cover_letter_endpoint():
 
-    # company_name = request.form.get('company_name')
-    # resume = request.files['resume']
+    company_name = request.form.get('company_name')
+    resume = request.files['resume']
 
-    # filename = secure_filename(resume.filename)
-    # file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    # resume.save(file_path)
+    filename = secure_filename(resume.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    resume.save(file_path)
 
-    # resume_text = extract_text_from_resume(file_path)
+    resume_text = extract_text_from_resume(file_path)
 
-    data = request.json 
+    # data = request.json 
 
-    company_name = data['company']
-    resume_text = data['resume']
-    position = data['position']
+    # company_name = data['company']
+    # resume_text = data['resume']
+    # position = data['position']
 
     # cover_letter = generate_cover_letter(company_name)
 
-    cover_letter = generate_cover_letter(company_name, resume_text, position)
+    cover_letter = generate_cover_letter(company_name, resume_text)
 
     # print(cover_letter)
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,20 +1,20 @@
 async function generateCoverLetter() {
     const companyName = document.getElementById('companyName').value;
-    const resume = document.getElementById('resume').value;
+    // const resume = document.getElementById('resume').value;
     const position = document.getElementById('position').value;
-    // const resumeFile = document.getElementById('resume').files[0];
+    const resumeFile = document.getElementById('resume').files[0];
     const resultDiv = document.getElementById('result');
 
-    // const formData = new FormData();
-    // formData.append('company_name', companyName);
-    // formData.append('resume', resumeFile);
+    const formData = new FormData();
+    formData.append('company_name', companyName);
+    formData.append('resume', resumeFile);
 
     const response = await fetch('/generate-cover-letter', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({company: companyName, resume: resume, position: position}) 
+        body: formData 
     });
 
     const data = await response.json();
